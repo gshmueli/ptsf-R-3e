b128d1fbcfb43537e19fa5dfda4125a7fb69e9f7
Galit Shmueli <43018858+gshmueli@users.noreply.github.com>
2023-12-17 15:07:01 +0800

    Update Ch09_Table 9.2.R
    Updated to include fable.binary LOGISTIC

--- Code/Ch09_Table 9.2.R
+++ Code/Ch09_Table 9.2.R
@@ -1,25 +1,54 @@
+### Table 9.1
+
+# create columns for trend and lag
+rain <- rain |>
+  mutate(
+    Rainy = (rain$RainfallAmount_millimetres > 0),  # create new binary column Rainy
+    Lag1 = lag(Rainy),        # create Lag1 with previous day Rainy/not
+    t = row_number(),   # create t with time index 1 to nPeriods
+    Seasonal_sine = sin(2 * pi * t / 365.25),   # create Seasonal_sine with sine term
+    Seasonal_cosine = cos(2 * pi * t / 365.25)  # create Seasonal_cosine with cosine term
+  )
+
+# Partition data
+train.rain <- rain |> filter_index(~ "2009-12-31")
+valid.rain <- rain |> filter_index("2010-01-01" ~ .)
+
+library(fable.binary) # for fitting logistic regression to tsibble object
+
+fit <- train.rain |>
+  model(
+    logistic = LOGISTIC(Rainy ~ Lag1 + Seasonal_sine + Seasonal_cosine)
+  )
+
+report(fit)
+fitted(fit) # gives predicted probabilities for training period
+fc <- forecast(fit, valid.rain) # predicted probabilities for validation period (in .mean)
+
+# training performance
+library(caret)
+confusionMatrix(data = as.factor(fitted(fit)$.fitted > 0.5),
+                reference = as.factor(train.rain$Rainy),
+                positive = "TRUE")
+
+# validation performance
+confusionMatrix(data = as.factor(fc$.mean > 0.5),
+                reference = as.factor(valid.rain$Rainy),
+                positive = "TRUE")
+
+# Alternative: use glm() in stat package
+train.rain.df <- as.data.frame(train.rain)
+fit.glm <- glm(Rainy ~ Lag1 + Seasonal_sine + Seasonal_cosine, data = train.rain.df, family = "binomial")
+
+summary(fit.glm) # estimated logistic model
+fitted(fit.glm) # predicted probabilities for training period
+
+valid.rain.df <- as.data.frame(valid.rain)
+pred.glm <- predict(fit.glm, valid.rain.df, type = "response") # type="response" gives probabilities
+
+# Performance
 library(caret)  # loading the package caret that contains functions to streamline the model training process for complex regression.
 library(InformationValue) # 
-
-rain.df <- read.csv("Data/MelbourneRainfall.csv") # read the data. The data frame contain two columns: Date and RainfallAmount_millimetres
-rain.df$Date <- as.Date(rain.df$Date, format="%d/%m/%Y") # change the data format in the Date column.
-rain.df$Rainy <- ifelse(rain.df$Rainfall > 0, 1, 0) # create new binary column Rainy (1=rainy day).
-nPeriods <- length(rain.df$Rainy) # number of rainy days 
-rain.df$Lag1 <- c(NA,rain.df$Rainfall[1:(nPeriods-1)])
-
-rain.df$t <- seq(1, nPeriods, 1)
-rain.df$Seasonal_sine = sin(2 * pi * rain.df$t / 365.25)
-rain.df$Seasonal_cosine = cos(2 * pi * rain.df$t / 365.25)
-
-train.df <- rain.df[rain.df$Date <= as.Date("31/12/2009", format="%d/%m/%Y"), ]
-train.df <- train.df[-1,]
-valid.df <- rain.df[rain.df$Date > as.Date("31/12/2009", format="%d/%m/%Y"), ]
-xvalid <- valid.df[, c(4,6,7)]
-
-rainy.lr <- glm(Rainy ~ Lag1 + Seasonal_sine + Seasonal_cosine, data = train.df, family = "binomial")
-summary(rainy.lr)
-
-rainy.lr.pred <- predict(rainy.lr, xvalid, type = "response") # type="response" gives probabilities
 
 confusionMatrix(ifelse(rainy.lr$fitted > 0.5, 1, 0), train.df$Rainy)
 confusionMatrix(ifelse(rainy.lr.pred > 0.5, 1, 0), valid.df$Rainy)
