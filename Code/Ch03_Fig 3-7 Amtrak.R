020e1ca9d77e493a5a8d23dddeebb381d206b4e8
Galit Shmueli <43018858+gshmueli@users.noreply.github.com>
2023-12-29 16:51:36 +0800

    Update Ch03_Fig 3-7 Amtrak.R

--- Code/Ch03_Fig 3-7 Amtrak.R
+++ Code/Ch03_Fig 3-7 Amtrak.R
@@ -4,7 +4,7 @@
 Amtrak.data <- read.csv("Data/Amtrak data.csv")
 
 ridership <- Amtrak.data |>
-  mutate(Month = yearmonth(as.character( Amtrak.data$Month))) |>
+  mutate(Month = yearmonth(as.character(Amtrak.data$Month))) |>
   as_tsibble(index = Month)
 
 train.ridership <- ridership |> filter_index( ~ "2001 Mar") 
