a618f344f79f5d200f6ac4ea8704360aca9bb425
Galit Shmueli <43018858+gshmueli@users.noreply.github.com>
2023-12-30 15:33:54 +0800

    Update Ch03_Fig 3-1 Amtrak.R

--- Code/Ch03_Fig 3-1 Amtrak.R
+++ Code/Ch03_Fig 3-1 Amtrak.R
@@ -11,7 +11,7 @@ pdf("Plots/AmtrakFig_3_1_3e.pdf",height=4,width=6)
 ridership |>
   autoplot(Ridership) +
   xlab("Time") + ylab("Ridership")  +
-  geom_vline(xintercept= as.numeric(as.Date(yearmonth("2001-April"))), linetype="solid", color = "grey55", size=0.6)+
+  geom_vline(xintercept = as.numeric(as.Date(yearmonth("2001-April"))), linetype = "solid", color = "grey55", size = 0.6)+
   geom_segment(aes(x = yearmonth("2001-May"), y = 2250, 
                    xend = yearmonth("2004-Mar"), yend = 2250),
                arrow = arrow(length = unit(0.25, "cm"), ends = "both") , size = 0.3, color = "grey55")+ 
