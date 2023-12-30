ac97207b22e23eec8d99b28ae8f5eb5695a3e78c
Galit Shmueli <43018858+gshmueli@users.noreply.github.com>
2023-12-30 15:53:31 +0800

    Update Ch03_Fig 3-7 Amtrak.R

--- Code/Ch03_Fig 3-7 Amtrak.R
+++ Code/Ch03_Fig 3-7 Amtrak.R
@@ -10,34 +10,54 @@ ridership <- Amtrak.data |>
 train.ridership <- ridership |> filter_index(~ "2001 Mar") 
 valid.ridership <- ridership |> filter_index("2001 Apr" ~ .)
 
-# Naive forecasts
-ridership.naive <- train.ridership |>
-  model(naive_model = NAIVE(Ridership)) |> 
-  forecast(h = "3 years") 
+fit <- train.ridership |>
+  model(
+    naive_model = NAIVE(Ridership),
+    snaive_model = SNAIVE(Ridership)
+  )
 
-# Seasonal naive forecasts
-ridership.snaive <- train.ridership |>
-  model(snaive_model = SNAIVE(Ridership)) |> 
-  forecast(h = "3 years") 
+# Get forecasts for validation period
+fc <- fit |>
+  forecast(h = nrow(valid.ridership)) # or simply h = "3 years"
 
-fc <- 
-train.ridership |>
-  autoplot(Ridership) +
-  autolayer(ridership.naive, level = NULL, linetype = "dashed", size = 1.25) +
-  autolayer(ridership.snaive, level = NULL, linetype = "dashed", color = "coral1", size = 0.8) +
+p.model <- autoplot(ridership, Ridership) +
+  # autolayer(fitted(fit), .fitted, alpha = 0.7) + # Uncomment to see training fitted values
+  autolayer(fc, .mean, level = NULL, linetype = "longdash", size = 1.25) +
+  scale_x_yearmonth(date_breaks = "2 years", date_labels = "%Y") +
   xlab("Time") + ylab("Ridership") +
-  geom_vline(xintercept= as.numeric(as.Date(yearmonth("2001-April"))), linetype="solid", color = "grey55", size=0.6)+
+  geom_vline(xintercept= as.numeric(as.Date(yearmonth("2001-April"))), linetype = "solid", color = "grey55", size = 0.6) +
   geom_segment(aes(x = yearmonth("2001-May"), y = 2250, 
                    xend = yearmonth("2004-Mar"), yend = 2250),
-               arrow = arrow(length = unit(0.25, "cm"), ends = "both") , size=0.3, color="grey55")+  # arrow Validation
-  annotate(geom="text", x=yearmonth("2002-Aug"), y=2280, label="Validation", color="grey37") +
-  
+               arrow = arrow(length = unit(0.25, "cm"), ends = "both") , color = "grey55") +  
+  annotate(geom = "text", x = yearmonth("2002-Aug"), y = 2280, label = "Validation", color = "grey37") +
   geom_segment(aes(x = yearmonth("1991-Jan"), y = 2250, 
                    xend = yearmonth("2001-Mar"), yend = 2250),
-               arrow = arrow(length = unit(0.25, "cm"), ends = "both"), size = 0.3, color = "grey55")+  # arrow Training
-  annotate(geom = "text", x = yearmonth("1996-Aug"), y = 2280, label = "Training", color = "grey37")+
-  scale_x_yearmonth(date_breaks = "2 years", date_labels = "%Y")
+               arrow = arrow(length = unit(0.25, "cm"), ends = "both"), color = "grey55") +  
+  annotate(geom = "text", x = yearmonth("1996-Aug"), y = 2280, label = "Training", color = "grey37") +
+  theme(legend.position = "bottom")
 
-pdf("Plots/AmtrakFig_3_3_3e.pdf",height=4,width=6)
-fc
-dev.off()
+# Errors plot
+fc2 <- fc |>
+  left_join(valid.ridership, by = "Month") |>
+  mutate(fc.error = valid.ridership$Ridership - fc$.mean)
+
+p.errors <- autoplot(fc2, fc.error, linetype = "dashed") +
+  autolayer(resid(fit), .resid) +
+  labs(title = "Errors", x = "Time", y = "Error") +
+  scale_x_yearmonth(date_breaks = "2 years", date_labels = "%Y") +
+#  theme(legend.position = "bottom") +
+  geom_vline(xintercept= as.numeric(as.Date(yearmonth("2001-April"))), linetype = "solid", color = "grey55", size = 0.6) +
+  geom_segment(aes(x = yearmonth("2001-May"), y = 500, 
+                   xend = yearmonth("2004-Mar"), yend = 500),
+               arrow = arrow(length = unit(0.25, "cm"), ends = "both") , color = "grey55") +  
+  annotate(geom = "text", x = yearmonth("2002-Aug"), y = 530, label = "Validation", color = "grey37") +
+  geom_segment(aes(x = yearmonth("1991-Jan"), y = 500, 
+                   xend = yearmonth("2001-Mar"), yend = 500),
+               arrow = arrow(length = unit(0.25, "cm"), ends = "both"), color = "grey55") +  
+  annotate(geom = "text", x = yearmonth("1996-Aug"), y = 530, label = "Training", color = "grey37") +
+  theme(legend.position = "none")
+
+# Plot 
+pdf("Plots/AmtrakFig_3_7_3e.pdf", height=7, width=8)
+grid.arrange(p.model, p.errors, nrow = 2)
+dev.off() 
