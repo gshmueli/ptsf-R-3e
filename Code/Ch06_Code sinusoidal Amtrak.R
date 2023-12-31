a1156beaaaeb376113820eaf3c0323fed8a1c205
Galit Shmueli <43018858+gshmueli@users.noreply.github.com>
2023-12-31 15:35:47 +0800

    Update and rename Ch06_Table 6.7 Amtrak.R to Ch06_Code sinusoidal Amtrak.R

---
+++ Code/Ch06_Code sinusoidal Amtrak.R
@@ -0,0 +1,16 @@
+################
+# Code for fitting sinusoidal seasonality
+
+Amtrak.data <- read.csv("Data/Amtrak data.csv")
+
+ridership <- Amtrak.data |>
+  mutate(Month = yearmonth(as.character( Amtrak.data$Month))) |>
+  as_tsibble(index = Month)
+
+train.ridership <- ridership |> filter_index(~ "2003 Mar") 
+valid.ridership <- ridership |> filter_index("2003 Apr" ~ .)
+
+# Sinusoidal seasonality & quadratic trend
+# Note: fourier(K=1) is equivalent to using I(sin(2*pi*trend()/12)) + I(cos(2*pi*trend()/12)) 
+train.ridership |> 
+  model(TSLM(Ridership ~ trend() + I(trend()^2) + fourier(K=1)) |> report()
