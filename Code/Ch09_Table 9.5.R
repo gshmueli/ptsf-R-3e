34e88a95ccb21f7f0f098db161af61d04f0a3808
Galit Shmueli <43018858+gshmueli@users.noreply.github.com>
2024-01-01 22:36:33 +0800

    Update Ch09_Table 9.5.R

--- Code/Ch09_Table 9.5.R
+++ Code/Ch09_Table 9.5.R
@@ -4,6 +4,7 @@ fit <- train.rain |>
   model(
     nn = BINNET(Rainy ~ Lag1 + Seasonal_sine + Seasonal_cosine) 
   )
+# Or, equivalently, use the simpler code: BINNET(Rainy ~ Lag1 + fourier(K = 1, period = "year"))
 
 report(fit)
 fitted(fit) # gives predicted probabilities (between 0 and 1)
