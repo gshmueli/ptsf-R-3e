10d8f4a3d8b757bc3501353d8b13c6243be27840
Galit Shmueli <43018858+gshmueli@users.noreply.github.com>
2024-02-07 19:14:53 +0800

    Update Ch03_Fig 3-6 Tumblr.R

--- Code/Ch03_Fig 3-6 Tumblr.R
+++ Code/Ch03_Fig 3-6 Tumblr.R
@@ -37,25 +37,25 @@ fc.ets.MMdN <- fit.ets.MMdN |>
   forecast(h = 115)
 
 # Plot
-AAN.polt <- fc.ets.AAN |>
+AAN.plot <- fc.ets.AAN |>
   autoplot(people, level = c(20,40,60,80)) +
  # geom_line(aes(y = .fitted, colour = "Fitted"), data = augment(fit.ets.AAN)) +
   ylab("World wide population (millions)") + xlab("Month") +
   theme(legend.position = "none") + 
   ylim(0,1200)
 
-MMN.polt <- fc.ets.MMN |>
+MMN.plot <- fc.ets.MMN |>
   autoplot(people, level = c(20,40,60,80)) +
   ylab("World wide population (millions)") + xlab("Month") +
   theme(legend.position = "none") + 
   ylim(0,1200)
 
-MMdN.polt <- fc.ets.MMdN |>
+MMdN.plot <- fc.ets.MMdN |>
   autoplot(people, level = c(20,40,60,80)) +
   ylab("World wide population (millions)") + xlab("Month") +
   theme(legend.position = "none") + 
   ylim(0,1200)
 
 pdf("Plots/TumblrFig_3_7_3e.pdf",height=5.2,width=8)
-grid.arrange(AAN.polt, MMN.polt, MMdN.polt, ncol = 3)
+grid.arrange(AAN.plot, MMN.plot, MMdN.plot, ncol = 3)
 dev.off()
